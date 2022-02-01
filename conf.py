1baa902606ca991b0fd5c1d0aa3ab135e8841d91
bmp-dot <bmpeterson@gmail.com>
2022-02-01 05:40:15 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 
-html_theme = ["sphinxawesome_theme"]
+html_theme = 'sphinxawesome_theme'
 #html_theme = 'alabaster'
 #html_theme = 'bootstrap'
 #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
