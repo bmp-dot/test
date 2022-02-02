ede7fbf4919dda1bd091ffb95cd8271fcdfdd207
bmp-dot <bmpeterson@gmail.com>
2022-02-01 17:32:20 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -74,7 +74,8 @@ latex_elements = {
 #
 
 #html_theme = 'sphinxawesome_theme'
-#html_theme = 'alabaster'
+
+html_theme = 'alabaster'
 
 #html_theme = 'bootstrap'
 #html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
@@ -82,8 +83,8 @@ latex_elements = {
 #html_theme = 'sphinx_pdj_theme'
 #html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
