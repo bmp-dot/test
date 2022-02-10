d5bce7cff94f2259f1c8d03af6f9ad7b6888906c
bmp-dot <bmpeterson@gmail.com>
2022-02-09 18:41:44 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -73,18 +73,11 @@ latex_toplevel_sectioning = 'section'
 # a list of builtin themes.
 #
 
-#html_theme = 'sphinxawesome_theme'
-
-#html_theme = 'alabaster'
 
 html_theme = 'bootstrap'
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
-#html_theme = 'sphinx_pdj_theme'
-#html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
 
-#html_theme_path = guzzle_sphinx_theme.html_theme_path()
-#html_theme = 'guzzle_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
